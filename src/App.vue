779a085a2063371840f0103c2e1cb634e1cc3e97
Luinil <61002225+Flolink1er@users.noreply.github.com>
2026-06-03 21:27:44 +0200

    refactor: update App.vue to include GameBox component and manage brut game state feat: enhance PlayerCard component to display player position and improve layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script>
-import MainMenu from './components/landing/MainMenu.vue'
+import MainMenu from './components/landing/MainMenu.vue';
+import GameBox from './components/GameBox.vue';
 
 export default {
   components: {
     MainMenu,
+    GameBox,
   },
+
+  data() {
+    return {
+      // game state
+      gameLauched: false,
+    }
 }
 </script>
 
 <template>
-  <MainMenu />
+  <MainMenu v-if="!gameLauched" />
+  <GameBox v-else />
 </template>
 
 <style scoped></style>
--- src/components/ui/PlayerCard.vue
+++ src/components/ui/PlayerCard.vue
@@ -1,8 +1,63 @@
 <!-- Template of a card (only use for displaying player info but could be used for more in the future) -->
+<script>
+export default {
+  props: {
+    player: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
 <template>
-  <div></div>
+  <div class="player-card">
+    <div class="player-color" :class="player.color"></div>
+
+    <div class="player-info">
+      <h3>{{ player.name }}</h3>
+
+      <p>Position : {{ player.position }}</p>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<style scoped>
+.player-card {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem;
+  background-color: #333;
+  border-radius: 8px;
+}
 
-<style scoped></style>
+.player-color {
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+}
+
+.player-color.red {
+  background-color: red;
+}
+.player-color.blue {
+  background-color: blue;
+}
+.player-color.yellow {
+  background-color: yellow;
+}
+.player-color.green {
+  background-color: green;
+}
+
+.player-info h3 {
+  margin: 0;
+  color: white;
+}
+
+.player-info p {
+  margin: 0.5rem 0;
+  color: lightgray;
+}
+</style>
