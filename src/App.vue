0c2de6a015629b70f2a5130f9832836b5504e68a
Luinil <61002225+Flolink1er@users.noreply.github.com>
2026-06-03 21:07:39 +0200

    create Modal component template
    Create Mainmenu component template
    
    Import component Mainmenu in App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup></script>
+<script>
+import MainMenu from './components/landing/MainMenu.vue'
+
+export default {
+  components: {
+    MainMenu,
+  },
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <MainMenu />
 </template>
 
 <style scoped></style>
