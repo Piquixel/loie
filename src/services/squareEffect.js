012037db34f139767510a5b670a7cc1251b1ee63
Piquixel <thomas.fetu2004@gmail.com>
2026-06-11 20:24:17 +0200

    improved GameSquares

--- src/services/squareEffect.js
+++ src/services/squareEffect.js
@@ -1,1 +1,16 @@
 //Manage case effects
+
+export const squareEffects = {
+  oie: (player) => {
+    console.log(`Case Oie! Le joueur ${player.name} double son lancer.`)
+  },
+  pont: () => {
+    console.log('Pont! Téléportation à la case 12.')
+  },
+  labyrinthe: () => {
+    console.log('Labyrinthe! Recul de 12 cases.')
+  },
+  mort: () => {
+    console.log('Tête de Mort! Retour à la case 0.')
+  },
+}
