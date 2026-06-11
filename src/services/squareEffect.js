521c5c309eae5f6e2edc2c36b0b994e92edb5efb
Piquixel <thomas.fetu2004@gmail.com>
2026-06-11 21:35:07 +0200

    added square effect methods

--- src/services/squareEffect.js
+++ src/services/squareEffect.js
@@ -3,14 +3,19 @@
 export const squareEffects = {
   oie: (player) => {
     console.log(`Case Oie! Le joueur ${player.name} double son lancer.`)
+    const displacementValue = player.position - player.lastPosition
+    player.position += displacementValue
   },
-  pont: () => {
+  pont: (player) => {
     console.log('Pont! Téléportation à la case 12.')
+    player.position = 12
   },
-  labyrinthe: () => {
+  labyrinthe: (player) => {
     console.log('Labyrinthe! Recul de 12 cases.')
+    player.position -= 12
   },
-  mort: () => {
+  mort: (player) => {
     console.log('Tête de Mort! Retour à la case 0.')
+    player.position = 0
   },
 }
