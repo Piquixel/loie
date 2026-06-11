594a3ba2f365c0e54ebcec991dfa8b87ad8f8b10
Piquixel <thomas.fetu2004@gmail.com>
2026-06-11 20:58:41 +0200

    created storage management methods

--- src/services/storageManager.js
+++ src/services/storageManager.js
@@ -1,1 +1,20 @@
-//Manage saves and loads to localStorage (Player AND GameState)
+// Manage saves and loads to localStorage (Player AND GameState)
+export class Storage {
+  dataKey = 'player_data'
+
+  save(data) {
+    const stringData = JSON.stringify(data)
+
+    localStorage.setItem(this.dataKey, stringData)
+  }
+
+  load() {
+    const data = localStorage.getItem(this.dataKey)
+    return JSON.parse(data)
+  }
+
+  clear() {
+    localStorage.removeItem(this.dataKey)
+    // localStorage.clear() // * Disabled for dev purposes
+  }
+}
